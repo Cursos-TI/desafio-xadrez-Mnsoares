2bbbc2d4bb40d1eae9fda8eca2eb342e18532a13
Mnsoares <mauriciosoares_1987@hotmail.com>
2025-05-16 16:03:58 +0000

    movimento da rainha baixo esquerda

--- xadrez.c
+++ xadrez.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
- void MoverTorre (int casas){ //Recursiva para o movimento do cavalo nivel mestre
+ void MoverTorre (int casas){ //Recursiva para o movimento do Torre nivel mestre
     if (casas > 0)
     {
         printf("Cima \n"); // Imprime o nome
         MoverTorre (casas -1);
     } 
 }
+
+void MoverBispo(char bisp[20], int movimento, int total) { //Recursiva para o movimento do Bispo nivel mestre
+    if (movimento <= total) {
+        printf("%s\n", bisp);
+        MoverBispo(bisp, movimento + 1, total);
+    }
+}
+//MOVIMENTO DA RAINHA
+void MovimentoRainha(char *nome, int contador) { // poderia utilizar o mesmo codigo para movimentar o cavalo L
+    if (contador <= 0) {
+        return; // Condição de base: quando o contador for 0, a função para
+    }
+
+    printf("%s", nome); // Imprime o nome
+    MovimentoRainha(nome, contador - 1); // Chamada recursiva, reduzindo o contador
+}
+
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
@@ -17,18 +34,19 @@ int main() {
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
      
       
-   int contador = 0;
+   int Bispo = 0;
     char nome[20] = "Diagonal"; // Assume um nome com no máximo 20 caracteres
 
     do {
         printf("%s\n", nome); // Imprime o nome
-        contador++; // Incrementa o contador
-    } while (contador < 5); // Condição para parar de imprimir
+        Bispo++; // Incrementa o contador
+    } while (Bispo < 5); // Condição para parar de imprimir
 
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
- int a = 0;
+ 
+    int a = 0; //movimentação da torre utilizando WHILE
 
     while (a < 5)
     {
@@ -37,7 +55,8 @@ int main() {
         a++;
     }
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+ // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+
  for (int i = 0; i < 5; i++)
     {
        printf("Baixo\n"); //movimento da rainha 5 vezes para baixo
@@ -73,7 +92,21 @@ int main() {
     MoverTorre(5);  // Movimento da torre iniciando com a recursiva 
     printf("Direita \n"); // Imprime o nome
 
+    char bisp[20] = "Diagonal"; // movimento a ser impresso 
+    int total_vezes = 5;
+
+    MoverBispo(nome, 1, total_vezes);
+
+    //MOVIMENTO DA RAINHA
+     char *nome1 = "Baixo\n"; // movimento a ser impresso
+     char *nome2 = "Esquerda "; // movimento a ser impresso
+
     
+    MovimentoRainha(nome1, 5); // Definição de vezes que o numero sera impresso
+   
+    MovimentoRainha(nome2, 2); // Definição de vezes que o numero sera impresso
+    printf("\n");
+
 
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
